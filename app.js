def7a8c387cced29baf9792072cab1c14d64e45a
Cokorda Raka Angga Jananuraga <dnsp.project@gmail.com>
2018-03-31 16:15:48 -0600

    some adjustments

--- app.js
+++ app.js
@@ -4,20 +4,20 @@
  * License: MIT
  */
 
-var path = require('path');
-var qs = require('querystring');
+const path = require('path');
+const qs = require('querystring');
 
-var bodyParser = require('body-parser');
-var cors = require('cors');
-var express = require('express');
-var jwt = require('jwt-simple');
-var moment = require('moment');
-var request = require('request');
-var UserRepository = require("./user-repository.js")
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const express = require('express');
+const jwt = require('jwt-simple');
+const moment = require('moment');
+const request = require('request');
+const UserRepository = require("./user-repository.js")
 
-var config = require('./config');
+const config = require('./config');
 
-var app = express();
+const app = express();
 
 app.set('port', process.env.NODE_PORT || 3000);
 app.set('host', process.env.NODE_IP || 'localhost');
@@ -27,8 +27,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Force HTTPS on Heroku
 if (app.get('env') === 'production') {
-  app.use(function(req, res, next) {
-    var protocol = req.get('x-forwarded-proto');
+  app.use((req, res, next) => {
+    const protocol = req.get('x-forwarded-proto');
     protocol == 'https' ? next() : res.redirect('https://' + req.hostname + req.url);
   });
 }
@@ -43,9 +43,8 @@ function ensureAuthenticated(req, res, next) {
   if (!req.header('Authorization')) {
     return res.status(401).send({ message: 'Please make sure your request has an Authorization header' });
   }
-  var token = req.header('Authorization').split(' ')[1];
-
-  var payload = null;
+  const token = req.header('Authorization').split(' ')[1];
+  let payload = null;
   try {
     payload = jwt.decode(token, config.JWT_SECRET);
   }
@@ -56,15 +55,16 @@ function ensureAuthenticated(req, res, next) {
   if (payload.exp <= moment().unix()) {
     return res.status(401).send({ message: 'Token has expired' });
   }
-  req.user = payload.sub;
+
+  req.userId = payload.sub;
   next();
 }
 
 function makePassword(length) {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let text = "";
+  const possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 
-  for (var i = 0; i < length; i++)
+  for (let i = 0; i < length; i++)
     text += possible.charAt(Math.floor(Math.random() * possible.length));
 
   return text;
@@ -76,8 +76,8 @@ function makePassword(length) {
  |--------------------------------------------------------------------------
  */
 function createJWT(user) {
-  var payload = {
-    sub: user.id,
+  const payload = {
+    sub: user._id,
     iat: moment().unix(),
     exp: moment().add(14, 'days').unix()
   };
@@ -89,8 +89,8 @@ function createJWT(user) {
  | GET /api/me
  |--------------------------------------------------------------------------
  */
-app.get('/api/me', ensureAuthenticated, function(req, res) {
-  UserRepository.findById(req.user).then(user => {
+app.get('/api/me', ensureAuthenticated, (req, res) => {
+  UserRepository.findById(req.userId).then(user => {
     res.send(user);
   });
 });
@@ -100,8 +100,8 @@ app.get('/api/me', ensureAuthenticated, function(req, res) {
  | PUT /api/me
  |--------------------------------------------------------------------------
  */
-app.put('/api/me', ensureAuthenticated, function(req, res) {
-  User.findById(req.user).then(user => {
+app.put('/api/me', ensureAuthenticated, (req, res) => {
+  User.findById(req.userId).then(user => {
     if (!user) {
       return res.status(400).send({ message: 'User not found' });
     }
@@ -118,7 +118,7 @@ app.put('/api/me', ensureAuthenticated, function(req, res) {
  | Log in with Email
  |--------------------------------------------------------------------------
  */
-app.post('/auth/login', function(req, res) {
+app.post('/auth/login', (req, res) => {
   UserRepository.findOne({ email: req.body.email }).then(user => {
     if (!user) {
       return res.status(401).send({ message: 'Invalid email and/or password' });
@@ -127,6 +127,7 @@ app.post('/auth/login', function(req, res) {
       if (!isMatch) {
         return res.status(401).send({ message: 'Invalid email and/or password' });
       }
+
       res.send({ token: createJWT(user) });
     });
   });
@@ -137,10 +138,10 @@ app.post('/auth/login', function(req, res) {
  | Login with GitHub
  |--------------------------------------------------------------------------
  */
-app.post('/auth/github', function(req, res) {
-  var accessTokenUrl = 'https://github.com/login/oauth/access_token';
-  var userApiUrl = 'https://api.github.com/user';
-  var params = {
+app.post('/auth/github', (req, res) => {
+  const accessTokenUrl = 'https://github.com/login/oauth/access_token';
+  const userApiUrl = 'https://api.github.com/user';
+  const params = {
     code: req.body.code,
     client_id: req.body.clientId,
     client_secret: config.GITHUB_SECRET,
@@ -148,53 +149,74 @@ app.post('/auth/github', function(req, res) {
   };
 
   // Step 1. Exchange authorization code for access token.
-  request.get({ url: accessTokenUrl, qs: params }, function(err, response, accessToken) {
+  request.get({ url: accessTokenUrl, qs: params }, (err, response, accessToken) => {
     accessToken = qs.parse(accessToken);
-    var headers = { 'User-Agent': 'Satellizer' };
+    const headers = { 'User-Agent': 'Satellizer' };
 
     // Step 2. Retrieve profile information about the current user.
-    request.get({ url: userApiUrl, qs: accessToken, headers: headers, json: true }, function(err, response, profile) {
-
+    request.get({ 
+      url: userApiUrl, qs: accessToken, headers: headers, json: true 
+    }, (err, response, profile) => {
       // Step 3a. Link user accounts.
+
       if (req.header('Authorization')) {
-        UserRepository.findOne({ github: profile.id }).then(existingUser => {
+        // This is for the case when user logs-in with email first, and then later decides to link with github
+        UserRepository.findOne({github: {profile: {id: profile.id }}}).then(existingUser => {
           if (existingUser) {
             return res.status(409).send({ message: 'There is already a GitHub account that belongs to you' });
           }
-          var token = req.header('Authorization').split(' ')[1];
-          var payload = jwt.decode(token, config.JWT_SECRET);
-          UserRepository.findById(payload.sub).then(user => {
+          const token = req.header('Authorization').split(' ')[1];
+          const payload = jwt.decode(token, config.JWT_SECRET);
+          const userId = payload.sub
+
+          UserRepository.findById(userId).then(user => {
             if (!user) {
               return res.status(400).send({ message: 'User not found' });
             }
-            user.github = profile.id;
-            user.picture = user.picture || profile.avatar_url;
-            user.displayName = user.displayName || profile.name;
+
+            user.github = {
+              profile: {
+                id: profile.id,
+                picture: profile.avatar_url,
+                displayName: profile.name
+              },
+              pass: accessToken
+            }
             UserRepository.save(user).then(() => {
-              var token = createJWT(user);
+              const token = createJWT(user);
               res.send({ token: token });
             });
           });
         });
       } else {
+        // This is for the case when user logs-in directly using his github account
         // Step 3b. Create a new user account or return an existing one.
-        UserRepository.findOne({ github: profile.id }).then(existingUser => {
+        UserRepository.findOne({ github: {profile: {id: profile.id}} }).then(existingUser => {
           if (existingUser) {
-            var token = createJWT(existingUser);
-            return res.send({ token: token });
+            existingUser.github.pass = accessToken 
+            UserRepository.save(existingUser).then(() => {
+              const token = createJWT(existingUser);
+              res.send({ token: token });
+            });
+          } else {
+            const temporaryPassword = makePassword(6)
+            UserRepository.create(profile.email, temporaryPassword).then(user => {
+              user.github = {
+                profile: {
+                  id: profile.id,
+                  picture: profile.avatar_url,
+                  displayName: profile.name
+                },
+                pass: accessToken
+              }
+              user.temporaryPassword = temporaryPassword
+    
+              UserRepository.save(user).then(() => {
+                const token = createJWT(user);
+                res.send({ token: token });
+              });
+            });
           }
-          
-          const temporaryPassword = makePassword(6)
-          var user = UserRepository.create(profile.email, temporaryPassword);
-          user.github = profile.id;
-          user.picture = profile.avatar_url;
-          user.displayName = profile.name;
-          user.temporaryPassword = temporaryPassword
-
-          UserRepository.save(user).then(() => {
-            var token = createJWT(user);
-            res.send({ token: token });
-          });
         });
       }
     });
@@ -206,15 +228,15 @@ app.post('/auth/github', function(req, res) {
  | Unlink Provider
  |--------------------------------------------------------------------------
  */
-app.post('/auth/unlink', ensureAuthenticated, function(req, res) {
-  var provider = req.body.provider;
-  var providers = ['github'];
+app.post('/auth/unlink', ensureAuthenticated, (req, res) => {
+  const provider = req.body.provider;
+  const providers = ['github'];
 
   if (providers.indexOf(provider) === -1) {
     return res.status(400).send({ message: 'Unknown OAuth Provider' });
   }
 
-  UserRepository.findById(req.user).then(user => {
+  UserRepository.findById(req.userId).then(user => {
     if (!user) {
       return res.status(400).send({ message: 'User Not Found' });
     }
@@ -231,7 +253,7 @@ app.post('/auth/unlink', ensureAuthenticated, function(req, res) {
  |--------------------------------------------------------------------------
  */
 require("./init-user-repository.js").then(() => {
-  app.listen(app.get('port'), app.get('host'), function() {
+  app.listen(app.get('port'), app.get('host'), () => {
     console.log('Express server listening on port ' + app.get('port'));
   });  
 })
